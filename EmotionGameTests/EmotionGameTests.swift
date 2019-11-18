80778e3f72c650f2c852f2c1efeb358215507521
zr018662 <hongwei1998>
2019-11-18 16:58:04 +0000

    Initial Commit

---
+++ EmotionGameTests/EmotionGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  EmotionGameTests.swift
+//  EmotionGameTests
+//
+//  Created by Edward Beckett on 18/11/2019.
+//  Copyright © 2019 Ed Beckett. All rights reserved.
+//
+
+import XCTest
+@testable import EmotionGame
+
+class EmotionGameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
